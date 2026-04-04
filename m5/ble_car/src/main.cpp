87cbe87435408ba1423cbf9168012c5eefabcbe3
bari <jabarilucien@gmail.com>
2026-04-03 22:00:16 -0400

    partial

--- m5/ble_car/src/main.cpp
+++ m5/ble_car/src/main.cpp
@@ -5,7 +5,8 @@
 #include "nsm_ble.h"
 #include "nsm_sender.h"
 
-
+// EXTERNAL VARIABLES
+extern int devicecount;
 
 // INSTANCES
 ESP_Pusher sender;
@@ -13,18 +14,29 @@ Bluetooth_Scanner ble;
 
 
 void setup() {
-  
+
   Serial.begin(115200);
+  delay(1000); // Wait for serial to stabilize
+
+  Serial.println("\n\n[+] Starting BLE Car Scanner...");
+
+  Serial.println("[*] Initializing BLE...");
   ble.setup();
+  Serial.println("[+] BLE initialized!");
+
   uint8_t masterAddr[] = {0x24, 0x6F, 0x28, 0xAA, 0xBB, 0xCC};
   sender.setReceiver(masterAddr);
-  sender.setup();
-  
+
+  // Serial.println("[*] Initializing ESP-NOW...");
+  // sender.setup();
+  Serial.println("[+] Setup complete!\n");
 
 }
 
 void loop() {
-  
+
+  Serial.println("[*] Starting BLE scan...");
   ble.Main(2000);
+  Serial.printf("[*] Scan complete. Found %d devices\n\n", devicecount);
 
 }
